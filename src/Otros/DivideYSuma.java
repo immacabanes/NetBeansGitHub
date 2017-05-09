2101c69e91fbdc9f34cef4c100c86139756f2cfa
Imma Cabanes <imma.cabanes@iesabastos.org>
2017-05-09 12:30:57 +0200

    1er commit

---
+++ src/Otros/DivideYSuma.java
@@ -0,0 +1,10 @@
+///Un programa que calcula una divisió i una suma.
+public class DivideYSuma {
+
+    public static void main(String[] args) {
+        double dividend = 20.0;
+        double divisor = 6.0;
+        double sumarAlFinal = 3.0;
+        System.out.println((dividend / divisor) + sumarAlFinal);
+    }
+}
